95cf0e408c615cb88f067d48bd12b56b37ec7569
Gougougogogo <394442302@qq.com>
2020-04-26 10:15:02 +0800

    add auto open feature on production env

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import open from 'open';
 import path from 'path';
 import boardExecutor from '../api/boardExecutor';
 import pipelineExecutor from '../api/pipelineExecutor';
@@ -22,4 +23,8 @@ app.get('/api/supported-source-control', (req, res) => {
 
 app.use('/',express.static(path.join(__dirname, sourcePath)));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
+
+if(process.env.MODE === 'PRODUCTION') {
+    open(`http://localhost:${port}`);
+}
